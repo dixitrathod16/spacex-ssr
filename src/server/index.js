4f71e55bec14fb93062d407b7eafe37089999e62
dixitrathod16 <14790427+dixitrathod16@users.noreply.github.com>
2020-10-09 22:17:08 +0530

    Add files via upload
    Latest Update

--- src/server/index.js
+++ src/server/index.js
@@ -41,8 +41,8 @@ app.get('*', (req, res, next) => {
         })
 })
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,() => {
-    console.log('Server is listening on port: 3000')
+    console.log('Server is listening on port: 8080')
 })
